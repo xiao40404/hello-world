3f6ce21d06e3e9e23127f169a9b3ed90c326cc75
xiao40404 <839524346@qq.com>
2018-10-04 23:25:51 +0800

    Create Two Sum.py

---
+++ Two Sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) <= 1:
+            return false
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+                #else:
+                    #continue continue用于while语句中
