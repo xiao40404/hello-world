c0788e2cfb3c361d36f9016df01918f67590df12
xiao40404 <839524346@qq.com>
2018-10-15 23:54:18 +0800

    return; dictionary

---
+++ Valid Parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for a in s:
+            if a == '(' or a == '[' or a =='{':
+                stack.append(a)
+            elif stack == []:
+                return False
+            elif stack != []:
+                if a == ')' and stack.pop() != '(':
+                    return False
+                elif a == ']' and stack.pop() != '[':
+                    return False
+                elif a == '}' and stack.pop() != '{':
+                    return False
+            else:
+                return False
+        return stack == []
