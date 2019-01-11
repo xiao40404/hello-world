55770c89720c3d180c98d2384eaafa340ee63e7b
xiao40404 <839524346@qq.com>
2019-01-12 00:34:57 +0800

    Create 67_Add_Binary.py

---
+++ 67_Add_Binary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        
+        if '' in (a,b):
+            return a or b
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[:-1],b[:-1]),'1')+'0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[:-1],b[:-1])+'0'
+        else:
+            return self.addBinary(a[:-1],b[:-1])+'1'
